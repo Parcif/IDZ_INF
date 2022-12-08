9c766458706ff1d5d709d7695b83e208d3ada08b
Parcif <arteminkin@yandex.ru>
2022-12-08 23:27:30 +0400

    Добавьте файлы проекта.

---
+++ ИДЗ взлом хеш функции.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include<iostream>
+#include<math.h>
+
+int main() {
+
+    vector<char> n;
+    vector <vector<char>> h;
+    //VARIABLES
+    char chars[] = { '0', '1', '2', '3' };
+    int password[4]{ 0 };
+
+    //SIZES OF VERIABLES
+    int chars_length = sizeof(chars) / sizeof(char);
+    int password_length = sizeof(password) / sizeof(int);
+
+    //CYCKLE TROUGH ALL OF THE COMBINATIONS
+    for (int i = 0; i < pow(chars_length, password_length); i++) {
+
+        //CYCKLE TROUGH ALL OF THE VERIABLES IN ARRAY
+        for (int i2 = 0; i2 < password_length; i2++) {
+            //IF VERIABLE IN "PASSWORD" ARRAY IS THE LAST VERIABLE IN CHAR "CHARS" ARRRAY
+            if (password[i2] == chars_length) {
+                //THEN INCREMENT THE NEXT VERIABLE IN "PASSWORD" ARRAY
+                password[i2 + 1]++;
+                //AND RESET THE VERIABLE BACK TO ZERO
+                password[i2] = 0;
+            }
+        }
+
+        //PRINT OUT FIRST COMBINATION
+        std::cout << i << ": ";
+        for (int i2 = 0; i2 < password_length; i2++) {
+            cout << chars[password[i2]] << " ";
+            n.push_back(chars[password[i2]]);
+        }
+        h.push_back(n);
+        n.clear();
+        std::cout << "\n";
+
+        //INCREMENT THE FIRST VERIABLE IN ARRAY
+        password[0]++;
+    }
+
+    cout << "\n_______________________________________________________________________\n" << endl;
+
+    for (int i = 0; i < h.size(); i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << h[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    char x1;
+    char x2;
+    char x3;
+    char x4;
+
+
+    for (int i = 0; i < h.size(); i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                for (int p = 0; p < 4; p++)
+                {
+                    for (int x = 0; x < 4; x++)
+                    {
+                        if ((h[i][j] * 1 + h[i][k] * 2 + h[i][p] * 4 + h[i][x] * 8) % 5 == 4)
+                        {
+                            x1 = h[i][j];
+                            x2 = h[i][k];
+                            x3 = h[i][p];
+                            x4 = h[i][x];
+
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    cout << "\n" << x1 << x2 << x3 << x4 << endl;
+    
+}
